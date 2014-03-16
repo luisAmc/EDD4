cc69d57a4487ea0fd1e3e8827cdd08a8e0819451
Luis Martinez <luis_amc@unitec.edu>
2014-03-16 09:43:37 +0100

    Con el Archivo de texto de Sistemas Computacionales

--- src/edd4/PlanDeClases.java
+++ src/edd4/PlanDeClases.java
@@ -11,5 +11,20 @@ package edd4;
  * @author Luis Martinez
  */
 public class PlanDeClases {
+    class NodoClase {
+        private String codigo;
+        private String nombre;
+        public NodoClase(String codigo, String nombre) {
+            this.codigo = codigo;
+            this.nombre = nombre;
+        }
+        public String toString() {
+            return codigo + " " + nombre;
+        }
+    }//Final de la clase anonimo NodoClase
+    private void cargarClases() {   
+    }
+    private void cargarDependencias() {
     
+    }
 }
