d08de7866d66f71bf45c9168f85dd8e3499f5517
Luis Martinez <luis_amc@unitec.edu>
2014-03-22 09:46:38 +0100

    Con los metodos cargarDependencias y cargarClases terminados.... creo

--- src/edd4/PlanDeClases.java
+++ src/edd4/PlanDeClases.java
@@ -7,9 +7,15 @@
 package edd4;
 
 import edu.uci.ics.jung.graph.SparseMultigraph;
+import edu.uci.ics.jung.graph.util.EdgeType;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -20,25 +26,78 @@ public class PlanDeClases {
     class NodoClase {
         private String codigo;
         private String nombre;
-        public NodoClase(String codigo, String nombre) {
+        private int uv;
+        private boolean semestral;
+        public NodoClase(String codigo, String nombre, int uv, boolean semestral) {
             this.codigo = codigo;
             this.nombre = nombre;
+            this.uv = uv;
+            this.semestral = semestral;
+        }
+        public String getCodigo() {
+            return codigo;
+        }
+        public int getUV() {
+            return uv;
         }
         public String toString() {
-            return codigo + " " + nombre;
+            return codigo + " " + nombre + " u.v.: " + uv + ((semestral == true) ? "Es una clase semestral" : "No es una clase semestral");
         }
-    }//Final de la clase anonimo NodoClase
+    }//Final de la clase anonima NodoClase
+    //Este metodo carga el codigo de la clase, su nombre, sus unidades valorativas y si es semestral o no
     private void cargarClases() {
         try {
-            File archivo = new File("./resources/Sistemas Computacionales.txt");
+            File archivo = new File("./resources/Sistemas Computacionales (Solo clases).txt");
             FileReader fr = new FileReader(archivo);
             BufferedReader br = new BufferedReader(fr);
-            String tmp;
+            String tmp, codigo, nombre, uv, semestral;
+            StringTokenizer tokens = null;
+            NodoClase nodo = null;
             while ((tmp = br.readLine()) != null) {
-                
+                tokens = new StringTokenizer(tmp, "@");
+                while (tokens.hasMoreTokens()) {
+                    codigo = tokens.nextToken();
+                    nombre = tokens.nextToken();
+                    uv = tokens.nextToken();
+                    semestral = tokens.nextToken();
+                   nodo = new NodoClase(codigo, nombre, Integer.parseInt(uv), (semestral.equalsIgnoreCase("true") ? true : false));
+                   grafo.addVertex(nodo);
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
+    }//Final del metodo que carga las clases (codigo, nombre, unidades valorativas, semestral)
+    
+    //Este metodo -cargarDependencias()- une los nodos del grafo
+    private void cargarDependencias() {
+        try {
+            File archivo = new File("./resources/Sistemas Computacionales (Dependencias).txt");
+            FileReader fr = new FileReader(archivo);
+            BufferedReader br = new BufferedReader(fr);
+            String tmp, codigoClase1, codigoClase2;
+            StringTokenizer tokens = null;
+            NodoClase clase1 = null;
+            NodoClase clase2 = null;
+            Iterator iteradorClases = grafo.getVertices().iterator();
+            while ((tmp = br.readLine()) != null){
+                tokens = new StringTokenizer(tmp, "@");
+                while (tokens.hasMoreTokens()){
+                    codigoClase1 = tokens.nextToken();
+                    codigoClase2 = tokens.nextToken();
+                    while (iteradorClases.hasNext()) {
+                        Object temporal = iteradorClases.next();
+                        if (((NodoClase)(temporal)).getCodigo().equalsIgnoreCase(codigoClase1)) {
+                            clase1 = (NodoClase) temporal;
+                        } else if (((NodoClase)(temporal)).getCodigo().equalsIgnoreCase(codigoClase2)) {
+                            clase2 = (NodoClase) temporal;
+                        }
+                    }
+                    grafo.addEdge("", clase1, clase2);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }//Final del metodo cargarDependencias(Este une los vertices del grafo)
 }
